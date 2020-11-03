0d7b3a6c90f876752c58b26a5fa21ab24cdf3675
Jacob Hillock <jacob@hillock.cc>
2020-11-03 14:43:32 -0500

    Basic implementation for api calling

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
+from api_requests import us_states, us_state_specific
 from os.path import isfile, join
 from json import loads, dumps
-import requests
+from time import sleep
+from requests import get as req_get
 
+def download_states_codes():
+    url = "https://raw.githubusercontent.com/michaelficarra/us-states/master/index.json"
+    with open("state_list.txt", "w+") as file:
+        r = req_get(url)
+        data = loads(r.text)
+        codes = list(data.keys())
+        military_keys = ['AA', 'AE', 'AP']
+        territory_keys = ['AS', 'DC', 'FM', 'GU', 'MH', 'MP', 'PR', 'PW', 'VI']
+        state_keys = [ key for key in codes if key not in military_keys+territory_keys ]
+        organized_data = {
+            "military_keys": military_keys,
+            "territory_keys": territory_keys,
+            "state_keys": state_keys
+        }
+        file.write(dumps(organized_data, indent='\t'))
 
-base_url_states = "https://data.covidactnow.org/latest/us/{STATE_CODE}.OBSERVED_INTERVENTION.timeseries.json"
-base_url_countries = "https://data.covidactnow.org/latest/us/counties/{COUNTRY_CODE}.OBSERVED_INTERVENTION.timeseries.json"
+def load_state_codes():
+    with open('state_list.txt') as file:
+        data = loads(file.read())
+    return data['state_keys']
+
+def write_ts ():
+    pass
+
+def write (data, file_name):
+    states = {}
+    for point in data:
+        hash_val = f"{point['state']}_{point['lastUpdatedDate']}"
+        states[hash_val] = {
+            "population": point["population"],
+            "metrics":    point["metrics"],
+            "riskLevels": point["riskLevels"],
+            "actuals":    point["actuals"]
+        }
+    open(file_name, 'w+').write(dumps(states, indent='\t'))
 
 
 
 def main():
-    if isfile('state_list.txt'):
-        url = "https://raw.githubusercontent.com/michaelficarra/us-states/master/index.json"
-        with open("state_list.txt", "w+") as file:
-            r = requests.get(url)
-            data = loads(r.text)
-            codes = list(data.keys())
-            military_keys = ['AA', 'AE', 'AP']
-            territory_keys = ['AS', 'DC', 'FM', 'GU', 'MH', 'MP', 'PR', 'PW', 'VI']
-            state_keys = [ key for key in codes if key not in military_keys+territory_keys ]
-            organized_data = {
-                "military_keys": military_keys,
-                "territory_keys": territory_keys,
-                "state_keys": state_keys
-            }
-            file.write(dumps(organized_data, indent='\t'))
+    if not isfile('state_list.txt'):
+        download_states_codes()
+    # data = us_states()
+    data = us_state_specific(use_ts=False)
+    write(data, 'temp.txt')
 
 
 if __name__ == '__main__':
